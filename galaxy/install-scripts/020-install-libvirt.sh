0bb2ff3138831600d1161264959f6f7c4b4ba912
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-21 23:44:39 +0000

    sudo virsh commands since groups have not been applied

--- galaxy/install-scripts/020-install-libvirt.sh
+++ galaxy/install-scripts/020-install-libvirt.sh
@@ -1,6 +1,6 @@
 sudo apt install -y libvirt-clients libvirt-daemon-system virtinst
 sudo usermod -aG libvirt rosen
 sudo usermod -aG libvirt-qemu rosen
-virsh net-start default
-virsh net-autostart default
+sudo virsh net-start default
+sudo virsh net-autostart default
 exit
