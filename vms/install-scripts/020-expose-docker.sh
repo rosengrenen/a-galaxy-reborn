fd6856bd1e7ce24ef1c1457fc37eee553fcb2867
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-21 13:58:54 +0000

    added source

--- vms/install-scripts/020-expose-docker.sh
+++ vms/install-scripts/020-expose-docker.sh
@@ -1,4 +1,6 @@
 sudo cp ../files/daemon.json /etc/docker/daemon.json
+
+# https://stackoverflow.com/questions/44052054/unable-to-start-docker-after-configuring-hosts-in-daemon-json
 sudo mkdir /etc/systemd/system/docker.service.d
 sudo cp ../files/override.conf /etc/systemd/system/docker.service.d/override.conf
 sudo systemctl restart docker
