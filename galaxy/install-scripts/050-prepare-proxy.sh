f86a0c06cfc33b588f18e019851c4062db2f5cb6
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-21 23:53:47 +0000

    assume that repo is cloned in /galaxy

--- galaxy/install-scripts/050-prepare-proxy.sh
+++ galaxy/install-scripts/050-prepare-proxy.sh
@@ -1,6 +1,2 @@
-sudo mkdir /galaxy
-sudo chown rosen:rosen /galaxy
-cd /galaxy
-git clone https://github.com/rosengrenen/a-galaxy-reborn
-cd a-galaxy-reborn/galaxy
+cd /galaxy/a-galaxy-reborn/galaxy
 cp .env.example .env
