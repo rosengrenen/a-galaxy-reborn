f6620bc56d26f7e905d10252b16abaf8951d5f0c
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-23 23:02:54 +0000

    use libvirt nss instead of dnsmasq hosts

--- galaxy/install-scripts/020-network-manager.sh
+++ galaxy/install-scripts/020-network-manager.sh
@@ -1,10 +1,3 @@
 sudo apt install -y network-manager
 sudo sed -i 's/version: 2/version: 2\n    renderer: NetworkManager/g' /etc/netplan/50-cloud-init.yaml
 sudo netplan apply
-
-sudo rm /usr/lib/NetworkManager/conf.d/10-dns-resolved.conf
-sudo cp /galaxy/a-galaxy-reborn/galaxy/conf-files/NetworkManager/00-dns-dnsmasq.conf /usr/lib/NetworkManager/conf.d/
-sudo cp /galaxy/a-galaxy-reborn/galaxy/conf-files/dnsmasq/00-nameservers.conf /etc/NetworkManager/dnsmasq.d/
-sudo cp /galaxy/a-galaxy-reborn/galaxy/conf-files/dnsmasq/10-addn-hosts.conf /etc/NetworkManager/dnsmasq.d/
-sudo cp /galaxy/a-galaxy-reborn/galaxy/conf-files/dnsmasq/hosts.dnsmasq /etc/
-sudo systemctl restart NetworkManager
--- galaxy/install-scripts/021-libvirt.sh
+++ galaxy/install-scripts/021-libvirt.sh
@@ -1,3 +1,6 @@
 sudo apt install -y libvirt-clients libvirt-daemon-system virtinst
 sudo virsh net-start default
 sudo virsh net-autostart default
+
+sudo apt install -y libnss-libvirt
+sudo sed -i 's/hosts.*/hosts:\t\tfiles libvirt libvirt_guest dns mymachines/g' /etc/nsswitch.conf
