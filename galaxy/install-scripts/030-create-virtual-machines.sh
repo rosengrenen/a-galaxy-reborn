b878c7b145115b8baa6e1f91a7532a66c5948bd9
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-24 00:30:54 +0000

    actually, do create a portainer vm

--- galaxy/install-scripts/030-create-virtual-machines.sh
+++ galaxy/install-scripts/030-create-virtual-machines.sh
@@ -3,6 +3,16 @@ wget https://releases.ubuntu.com/20.04.1/ubuntu-20.04.1-live-server-amd64.iso \
 
 sudo virt-install \
   --virt-type kvm \
+  --name portainer \
+  --memory 1024 --vcpus=1 \
+  --cdrom /tmp/ubuntu-20.04-server-amd64.iso \
+  --disk size=10 \
+  --graphics vnc,listen=0.0.0.0 \
+  --noautoconsole \
+  --autostart
+
+sudo virt-install \
+  --virt-type kvm \
   --name prod \
   --memory 2048 --vcpus=2 \
   --cdrom /tmp/ubuntu-20.04-server-amd64.iso \
