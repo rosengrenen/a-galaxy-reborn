f1a416568b393f09e06ede672ee65a6395d284f7
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-24 00:27:43 +0000

    corrected path

--- vms/install-scripts/020-expose-docker.sh
+++ vms/install-scripts/020-expose-docker.sh
@@ -1,7 +1,7 @@
-sudo cp /galaxy/a-galaxy-reborn/cms/conf-files/daemon.json /etc/docker/daemon.json
+sudo cp /galaxy/a-galaxy-reborn/vms/conf-files/daemon.json /etc/docker/daemon.json
 
 # https://stackoverflow.com/questions/44052054/unable-to-start-docker-after-configuring-hosts-in-daemon-json
 sudo mkdir /etc/systemd/system/docker.service.d
-sudo cp /galaxy/a-galaxy-reborn/cms/conf-files/override.conf /etc/systemd/system/docker.service.d/override.conf
+sudo cp /galaxy/a-galaxy-reborn/vms/conf-files/override.conf /etc/systemd/system/docker.service.d/override.conf
 sudo systemctl daemon-reload
 sudo systemctl restart docker
