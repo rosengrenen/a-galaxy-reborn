ff392799a365d5da5fa04554a588565e0a620f58
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-25 00:40:10 +0000

    copy instead of mount

--- vms/install-scripts/040-traefik-catchall-to-http.sh
+++ vms/install-scripts/040-traefik-catchall-to-http.sh
@@ -5,9 +5,13 @@ sudo docker run \
   --network traefik \
   --restart unless-stopped \
   --detach \
-  --volume /galaxy/a-galaxy-reborn/vms/config-files/nginx/nginx.conf:/etc/nginx/nginx.conf \
   --label "traefik.enable=true" \
   --label "traefik.http.routers.forward-to-http.rule=hostregexp(\`{host:.+}\`)" \
   --label "traefik.http.routers.forward-to-http.entrypoints=http" \
   --label "traefik.http.routers.forward-to-http.priority=1" \
   nginx:1.19.0
+sudo docker cp \
+  /galaxy/a-galaxy-reborn/vms/config-files/nginx/nginx.conf:/etc/nginx/nginx.conf \
+  nginx-traefik-catchall-to-http:/etc/nginx/nginx.conf
+sudo docker restart nginx-traefik-catchall-to-http
+
