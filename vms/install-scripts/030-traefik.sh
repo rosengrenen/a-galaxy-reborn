90f0be31214da7bc8ac759cf21c34c04042bfa88
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-11-25 00:35:56 +0000

    nginx traefik catchall

--- vms/install-scripts/030-traefik.sh
+++ vms/install-scripts/030-traefik.sh
@@ -3,7 +3,6 @@ if [ -z $domain ]; then
   domain="chalmers.it"
 fi
 
-cd /galaxy/a-galaxy-reborn/vms/conf-files/traefik
 export DOMAIN=$domain
 export TRAEFIK_STORAGE_PATH=/data/traefik
 
@@ -18,14 +17,9 @@ sudo docker run \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume "$TRAEFIK_STORAGE_PATH/logs:/var/log" \
   --label "traefik.enable=true" \
-  --label "traefik.http.routers.forward-to-http.rule=hostregexp(\`{host:.+}\`)" \
-  --label "traefik.http.routers.forward-to-http.entrypoints=http" \
-  --label "traefik.http.routers.forward-to-http.service=forward-to-http" \
   --label "traefik.http.routers.api.rule=Host(\`traefik.$DOMAIN\`)" \
   --label "traefik.http.routers.api.service=api@internal" \
   --label "traefik.http.routers.api.entryPoints=http" \
-  --label "traefik.http.services.forward-to-http.loadbalancer.server.url=http" \
-  --label "traefik.http.services.forward-to-http.loadbalancer.server.port=80" \
   traefik:v2.3.3 \
   --api.insecure=false \
   --api.dashboard=true \
@@ -43,5 +37,3 @@ sudo docker run \
   --providers.docker.exposedByDefault=false \
   --providers.docker.network=traefik \
   --entrypoints.http.address=:80
-
-      # - traefik.http.services.api.loadbalancer.server.port=8080
---
+++ vms/install-scripts/040-traefik-catchall-to-http.sh
@@ -0,0 +1,13 @@
+sudo docker stop nginx-traefik-catchall-to-http
+sudo docker rm nginx-traefik-catchall-to-http
+sudo docker run \
+  --name nginx-traefik-catchall-to-http \
+  --network traefik \
+  --restart unless-stopped \
+  --detach \
+  --volume /galaxy/a-galaxy-reborn/vms/config-files/nginx/nginx.conf:/etc/nginx/nginx.conf \
+  --label "traefik.enable=true" \
+  --label "traefik.http.routers.forward-to-http.rule=hostregexp(\`{host:.+}\`)" \
+  --label "traefik.http.routers.forward-to-http.entrypoints=http" \
+  --label "traefik.http.routers.forward-to-http.priority=1" \
+  nginx:1.19.0
